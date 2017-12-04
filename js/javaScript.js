84cf227b96ada936e10e15575aec22228438d937
bohdan92 <bogdanidze92@gmail.com>
2017-12-04 21:48:03 +0200

    kalkulyator_1

---
+++ js/javaScript.js
@@ -0,0 +1,25 @@
+function sum(a, b) {
+    var result = +a + +b;        
+   console.log(result);
+   alert(result);
+
+}
+//sum(1, 2);
+
+/**function multiplication(a, b) {
+    var result = a * b;
+    console.log(result);
+}
+multiplication(89, 10);
+
+function division(a, b) {
+    var result = a / b;
+    console.log(result);
+}
+division(56, 4);
+
+function subtraction(a, b) {
+    var result = a - b;
+    console.log(result);
+}
+subtraction(98, 5);**/
